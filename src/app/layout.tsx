0ec9b6d1a720cda2d4e03bf40fed065a0a93a421
Tellora <telloramedia@gmail.com>
2026-04-01 22:20:36 +0530

    gtag 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,6 +130,27 @@ export default function RootLayout({
             <body
                 className={`${plusJakartaSans.variable} ${outfit.variable} antialiased selection:bg-primary/20 overflow-x-hidden`}
             >
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MSRGVJ8L"
+                        height="0" 
+                        width="0" 
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
+
+                {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MSRGVJ8L');
+                    `}
+                </Script>
                 {/* Google tag (gtag.js) */}
                 <Script 
                     strategy="afterInteractive" 
